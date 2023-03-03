178ee0194456055a5ac73e10d41239593e4beebc
Endrews Azevedo <98989141+Endrewss@users.noreply.github.com>
2023-03-03 13:07:50 -0300

    Upload tasks.ts and Create index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { TaskList } from "./tasks";
+import "/style.css";
+
+const ul = document.getElementById('task__list');
+
+let myTask = new TaskList(ul);
--- tasks.ts
+++ tasks.ts
@@ -3,8 +3,10 @@ export class TaskList{
     list: string[] = [];
 
     constructor(zawarudo: HTMLElement){
+        this.list = ['task 1','task 2','task 3'];
         this.ul = zawarudo;
         this.read();
+        this.render();
     }
 
     save(){
@@ -16,5 +18,24 @@ export class TaskList{
         if(saved){
             this.list = JSON.parse(saved);
         }
+    }
+
+    render(){
+        this.ul.innerHTML = "";
+        this.list.forEach((task) => {
+            const li = document.createElement("li");
+            const button = document.createElement("button");
+
+            button.innerText = "-";
+            button.addEventListener("click", ()=>{
+                const ls = this.list.indexOf(task);
+                if(ls > 0){
+                    this.list.splice(ls, 1);
+                }
+            });
+            li.innerText = task;
+            li.appendChild(button);
+            this.ul.appendChild(li);
+        });
     }
 }
